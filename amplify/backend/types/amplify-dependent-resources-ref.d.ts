4ddedfa2801de4712e7e22a2ee93bc5a68558219
oh <guruwang2@gmail.com>
2022-11-02 17:22:00 +0900

    인증 처리 완료

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,13 +1,1 @@
-export type AmplifyDependentResourcesAttributes = {
-    "auth": {
-        "authenticatordf03bc0b": {
-            "IdentityPoolId": "string",
-            "IdentityPoolName": "string",
-            "UserPoolId": "string",
-            "UserPoolName": "string",
-            "AppClientIDWeb": "string",
-            "AppClientID": "string",
-            "AppClientSecret": "string"
-        }
-    }
-}
+export type AmplifyDependentResourcesAttributes = {}
--- src/App.js
+++ src/App.js
@@ -1,48 +1,25 @@
 import React from 'react';
 import './App.css';
 import logo from './logo.svg';
-import {Amplify,Auth} from 'aws-amplify';
+import { Amplify, Auth } from 'aws-amplify';
 import {
 	AmplifyAuthenticator,
 	AmplifyContainer,
-	AmplifySignOut,
+	Authenticator,
+	useAuthenticator
 } from '@aws-amplify/ui-react';
 import { AuthState, onAuthUIStateChange } from '@aws-amplify/ui-components';
 import awsconfig from './aws-exports';
-
+import '@aws-amplify/ui-react/styles.css';
+import Login from "./Login";
 Amplify.configure(awsconfig);
 
 const App = () => {
-	const [authState, setAuthState] = React.useState();
-	const [user, setUser] = React.useState();
-
-	React.useEffect(() => {
-		
-		return onAuthUIStateChange((nextAuthState, authData) => {
-			setAuthState(nextAuthState);
-			setUser(authData);
-		});
-	}, []);
-
-	const signIn = async ()=>{
-		const res = await Auth.signIn("oh","1234");
-		console.log(res);
-	}
-
-	return authState === AuthState.SignedIn && user ? (
-		<div className="App">
-			<header className="App-header">
-				<div>Hello, {user.username}</div>
-				<img src={logo} className="App-logo" alt="logo" />
-				<AmplifySignOut />
-			</header>
-		</div>
-	) : (
-		// <button onClick={signIn}></button>
-		<AmplifyContainer>
-			<AmplifyAuthenticator />
-		</AmplifyContainer>
-	);
+	return (
+		<Authenticator.Provider>
+			<Login></Login>
+		</Authenticator.Provider>
+	)
 };
 
 export default App;
