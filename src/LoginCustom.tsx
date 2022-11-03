08c50bbf6662894c115d7dd13cef1bb2ff287b07
oh <guruwang2@gmail.com>
2022-11-03 12:36:27 +0900

    회원가입 로그인 인증 js 연동중

--- src/LoginCustom.tsx
+++ src/LoginCustom.tsx
@@ -7,6 +7,16 @@ import { Authenticator, useAuthenticator } from "@aws-amplify/ui-react";
 import { AuthState, onAuthUIStateChange } from "@aws-amplify/ui-components";
 import "@aws-amplify/ui-react/styles.css";
 
+type SignUpType = {
+  username: string;
+  password: string;
+  phone_number: string;
+};
+type SignInType = {
+  username: string;
+  password: string;
+};
+
 const LoginCustom = () => {
   const {
     register,
@@ -24,15 +34,55 @@ const LoginCustom = () => {
   console.log(route);
   console.log(user);
 
-  const onSubmit = (data) => {
+  const onSignUp = async (data: SignUpType) => {
     console.log("data : ", data);
+    const { user } = await Auth.signUp({
+      username: data.username,
+      password: data.password,
+      attributes: {
+        phone_number: "+82" + data.phone_number, // optional - E.164 number convention
+        // other custom attributes
+      },
+      autoSignIn: {
+        // optional - enables auto sign in after user is confirmed
+        enabled: true,
+      },
+    });
+    console.log("user : ", user);
+  };
+  const onSignIn = async (data: SignInType) => {
+    console.log("data : ", data);
+    try {
+      const user = await Auth.signIn(data.username, data.password);
+      console.log(user);
+    } catch (error) {
+      console.log("error signing in", error);
+    }
+  };
+  const onVerifyMFA = async (data: { code: string }) => {
+    const resMFA = await Auth.confirmSignIn(user, data.code, "SMS_MFA");
+    console.log(resMFA);
   };
   return (
     <>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <input type="text" placeholder="id" {...register("id" as any)} />
-        <input type="text" placeholder="pw" {...register("pw" as any)} />
+      <form onSubmit={handleSubmit((data: any) => onSignUp(data))}>
+        <input type="text" placeholder="id" {...register("username" as any)} />
+        <input type="text" placeholder="pw" {...register("password" as any)} />
+        <input
+          type="text"
+          placeholder="01012345678"
+          {...register("phone_number" as any)}
+        />
         <input type="submit" value="submit" />
+      </form>
+      <form onSubmit={handleSubmit((data: any) => onSignIn(data))}>
+        <input type="text" placeholder="id" {...register("username" as any)} />
+        <input type="text" placeholder="pw" {...register("password" as any)} />
+        <input type="submit" value="submit" />
+      </form>
+      <form onSubmit={handleSubmit((data: any) => onVerifyMFA(data))}>
+        <input type="text" placeholder="000000" {...register("code" as any)} />
+        <input type="submit" value="auth" />
       </form>
     </>
   );
